82cf3e0d63a50294fe376b18d7a0acbd552f793f
Bard-Gaming <christophe@dronne.fr>
2025-10-05 20:23:09 +0200

    feat(page): add Minecraft Inventory to enchant lookup page

--- src/pages/MahouTsukai/EnchantLookup/EnchantLookup.tsx
+++ src/pages/MahouTsukai/EnchantLookup/EnchantLookup.tsx
@@ -1,17 +1,55 @@
+import Toast from "../../../components/Toast/Toast";
 import styles from './EnchantLookup.module.css';
 import lookupData from './data/lookup.json'
+import MinecraftInventory from "../../../components/MinecraftInventory/MinecraftInventory";
+import probabilityAlterImage from '../../../assets/probability_alter.png';
+import enchantedBookImage from '../../../assets/enchanted_book.gif';
 
 
 function EnchantLookup() {
-    console.log(lookupData);
+    const availableEnchants = getAvailableEnchants();
 
     return (
         <section className={styles.enchant_lookup}>
-            <h1>Mahou Tsukai Lookup</h1>
+            <MinecraftInventory
+                items={availableEnchants}
+                onClick={id => console.log(id)}
+            />
+
+            <div className={styles.lookup_result}>
+                <Toast title="Rule Breaker" icon={probabilityAlterImage}>
+                    <span>Dividend: {5}</span>
+                </Toast>
+            </div>
         </section>
     );
 }
 
+function getAvailableEnchants(): { id: string, name: string, icon: string }[] {
+    const availableEnchants = new Set<string>();
+
+    for (const itemClassData of Object.values(lookupData)) {
+        for (const dividendData of itemClassData) {
+            for (const enchantments of Object.values(dividendData.enchantments)) {
+                enchantments.forEach(enchantment => availableEnchants.add(enchantment.id));
+            }
+        }
+    }
+
+    return [...availableEnchants].map(id => ({
+        id,
+        name: displayName(id),
+        icon: enchantedBookImage,
+    }));
+}
+
+function displayName(id: string): string {
+    const words = id.split('_');
+
+    return words
+        .map(word => word.charAt(0).toUpperCase() + word.substring(1))
+        .join(' ');
+}
 
 export default EnchantLookup;
 
