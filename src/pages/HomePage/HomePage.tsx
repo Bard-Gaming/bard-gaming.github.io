041c6a9d3ba6663aee2294e1739088d0fb13af6b
Bard-Gaming <christophe@dronne.fr>
2025-10-05 17:44:55 +0200

    feat(page): add link to other button on homepage

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,9 +1,22 @@
-import styles from './HomePage.module.css';
+import Toast from "../../components/Toast/Toast";
+import enchantedBook from '../../assets/enchanted_book.gif';
+import { useContext } from "react";
+import CurrentPageContext from "../../hooks/CurrentPageContext";
+import MahouTsukaiLookupPage from "../MahouTsukaiLookupPage/MahouTsukaiLookupPage";
 
 function HomePage() {
+    const [_currentPage, setCurrentPage] = useContext(CurrentPageContext);
+
     return (
         <section>
-        </section>        
+            <Toast
+                icon={enchantedBook}
+                title="Mahou Tsukai"
+                onClick={() => setCurrentPage(<MahouTsukaiLookupPage />)}
+            >
+                Enchantment Probability Lookup
+            </Toast>
+        </section>
     );
 }
 
