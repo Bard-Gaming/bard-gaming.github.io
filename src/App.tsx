6809f33bdc4fd50faa433a1e7392cebb9d1a7304
Bard-Gaming <christophe@dronne.fr>
2025-10-05 16:31:47 +0200

    feat(app): update app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import './App.css'
+import './App.css';
+import { useState } from "react";
+import CurrentPageContext from "./hooks/CurrentPageContext";
+import NavigationBar from "./components/NavigationBar/NavigationBar";
+import HomePage from "./pages/HomePage/HomePage";
+
 
 function App() {
-  return (
-    <>
-        <h1>Hello World!</h1>
-    </>
-  )
+    const [currentPage, setCurrentPage] = useState(<HomePage/>);
+
+    return (
+        <>
+            <CurrentPageContext value={[currentPage, setCurrentPage]}>
+                <NavigationBar title="Bard-Gaming's Tools" />
+                {currentPage}
+            </CurrentPageContext>
+        </>
+    );
 }
 
 export default App
